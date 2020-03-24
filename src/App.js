c79cf6eb7d7db38f8e38a7a0f49b09c8a3212641
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-03-24 20:03:22 +0900

    setup graphql client

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import client from './client'
+// import { render } from '@testing-library/react';
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+const ME = gql`
+query me { 
+  user(login: "iteachonudemy") { 
+    name
+    avatarUrl
+  }
+}
+`
 
-console.log({GITHUB_TOKEN})
+class App extends Component {
+  
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>
+          hello!!
+        </div>
 
-function App() {
-  return (
-    <div>
-        hello!!
-    </div>
-  );
+        <Query query={ME}>
+          {
+            ({ loading, error, data }) => {
+              // loadingのスペルミスで[Legacy context API has been detected within a strict-mode tree.]等のエラー発生注意
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
+
+              return <div>{data.user.name}</div>
+            }
+          }
+        </Query>
+      </ApolloProvider>
+    )
+  }
 }
 
 export default App;
