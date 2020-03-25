c99ab734ba4961ccfab3519b3ee13b07ed64056d
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-03-25 13:34:56 +0900

    apply query and variavles

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import { ApolloProvider } from 'react-apollo'
 import { Query } from 'react-apollo'
 import client from './client'
 import { ME } from './graphql'
+import { SEARCH_REPOSITORES } from './graphql'
 
+const VARIABLES =
+{
+  " after": null,
+  "before": null,
+  "first": 5,
+  "last": null,
+  "query": "フロントエンドエンジニア"
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = VARIABLES
+  }
 
   render() {
+    const { query, first, last, before, after } = this.state
     return (
       <ApolloProvider client={client}>
-        <div>
-          hello!!
-        </div>
-
-        <Query query={ME}>
+        <Query
+          query={SEARCH_REPOSITORES}
+          variables={{ query, first, last, before, after }}>
           {
             ({ loading, error, data }) => {
               // loadingのスペルミスで[Legacy context API has been detected within a strict-mode tree.]等のエラー発生注意
               if (loading) return 'Loading...'
               if (error) return `Error! ${error.message}`
-
-              return <div>{data.user.name}</div>
+              console.log(data)
+              return <div></div>
             }
           }
         </Query>
